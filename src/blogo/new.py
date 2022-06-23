f843a17022b01e81ffd0bd718f39e8b41019ccdd
MSAdministrator <rickardja@live.com>
2022-06-23 11:28:59 -0500

    feat: Adding new class to create blog and posts

---
+++ src/blogo/new.py
@@ -0,0 +1,37 @@
+from attrs import fields
+
+from .base import Base
+from .models import Blog, Post
+
+
+class New(Base):
+
+    @property
+    def blog(self):
+        if self._blog:
+            continue_or_not = self.session.prompt(f"You already have a blog config for '{self._blog.title}'. "
+                                                   "Do you want to create a new blog? "
+                                                )
+            if not continue_or_not.lower().startswith('y'):
+                return
+        self._blog = Blog(
+            title=self.session.prompt(fields(Blog).title.metadata["question"].substitute()),
+            author=self.session.prompt(fields(Blog).author.metadata["question"].substitute()),
+            github_username=self.session.prompt(fields(Blog).github_username.metadata["question"].substitute()),
+            directory=self.session.prompt(fields(Blog).directory.metadata["question"].substitute()),
+        )
+        self.__logger.info(f"Your blogo configuration file is located at {self.save_blog_conf_to_disk()}")
+
+    @property
+    def post(self):
+        self.blog
+        post = Post(
+            title=self.session.prompt(fields(Post).title.metadata["question"].substitute()),
+            categories=self.session.prompt(fields(Post).categories.metadata["question"].substitute()),
+            tags=self.session.prompt(fields(Post).tags.metadata["question"].substitute()),
+            author=self.session.prompt(fields(Post).author.metadata["question"].substitute()),
+            date=self.session.prompt(fields(Post).date.metadata["question"].substitute())
+        )
+        path = self.save_post_template_to_disk(post=post)
+        self._blog.posts.append(post)
+        self.update_blog_conf_on_disk()
